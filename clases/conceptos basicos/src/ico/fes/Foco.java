7ddc1daa036ab096d62942fcd9d81f8ff2e65cad
PedroRuizS <pedroruizescuela@gmail.com>
2022-02-24 12:41:46 -0600

    iniciamos el archivo Foco.java

---
+++ clases/conceptos basicos/src/ico/fes/Foco.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ico.fes;
+
+/**
+ *
+ * @author pedrooooo
+ */
+public class Foco {
+ String color;
+ String tipo;
+ int tamano;
+ 
+ 
+}
